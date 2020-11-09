137a8ac3eac860a309be7c0dcab3e5ea5f48afe6
Pawan Kumar <pkumrbh@amazon.com>
2020-11-09 18:40:33 +0530

    Updates

---
+++ precache-manifest.5f5230a2a12facda9287c71bfc73f086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d72ec459721a9bfca299de6fc48497",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "745a3777c547b802357a",
+    "url": "/monsters-rolodex/static/css/main.69337370.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745a3777c547b802357a",
+    "url": "/monsters-rolodex/static/js/main.a7e5c232.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
